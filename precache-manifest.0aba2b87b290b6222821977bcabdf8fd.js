4efc707708dd13ff7ce79d107953e328c9f78ddb
Code Sandbox <hello@codesandbox.io>
2021-10-01 08:17:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.0aba2b87b290b6222821977bcabdf8fd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092f94bdb372e34f94f122d7cd8e17e4",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/index.html"
+  },
+  {
+    "revision": "e63b976a9335b77eba62",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/css/2.dfd58508.chunk.css"
+  },
+  {
+    "revision": "409f4b09054f508af904",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/css/main.a11625c5.chunk.css"
+  },
+  {
+    "revision": "e63b976a9335b77eba62",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/js/2.b97ad515.chunk.js"
+  },
+  {
+    "revision": "409f4b09054f508af904",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/js/main.71ec26b4.chunk.js"
+  },
+  {
+    "revision": "8238e3336b4a07a39ac7",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/js/runtime~main.19db059c.js"
+  },
+  {
+    "revision": "3f2a4daede60a2fd953c7dd31fa71972",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/burger.3f2a4dae.png"
+  },
+  {
+    "revision": "f5b6ed116c1a672edcfc8dd29bb7e69f",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/floor.f5b6ed11.png"
+  },
+  {
+    "revision": "65fd5b77ad79d908b38f1b913c392140",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/ground.65fd5b77.png"
+  },
+  {
+    "revision": "fc41d8c475c7cefe4e5e77b96a6a2e6f",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/houses.fc41d8c4.png"
+  },
+  {
+    "revision": "746323a4ffd8540c15beb78e0ff61eec",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/kitchen.746323a4.png"
+  },
+  {
+    "revision": "3f7c63c6eb55148311e9decba30db66f",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/picture.3f7c63c6.png"
+  },
+  {
+    "revision": "8af7211326cceb52ec43da113c9e988d",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/plate.8af72113.png"
+  },
+  {
+    "revision": "ab1e31dd399e53c8d035dc23fb34d878",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/room.ab1e31dd.png"
+  },
+  {
+    "revision": "c15ed25458fe27bae6e90bdb222360e0",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/room.c15ed254.png"
+  },
+  {
+    "revision": "cda649da851218c87e9595dead90e024",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/room.cda649da.png"
+  },
+  {
+    "revision": "08e91c434e24f6c3bd4c0a5e28756499",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/signboard.08e91c43.png"
+  },
+  {
+    "revision": "b17e6031047271a8bf57c6d4ea298ceb",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/table.b17e6031.png"
+  },
+  {
+    "revision": "aa4e1286ab429892c5fd0ec0b268dadb",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/winner.aa4e1286.png"
+  },
+  {
+    "revision": "6a9cdb6b1f93f38913529f0ee73d31f4",
+    "url": "https://yurytatsenka.github.io/csb-8scq7/static/media/wooden-floor.6a9cdb6b.png"
+  }
+]);
